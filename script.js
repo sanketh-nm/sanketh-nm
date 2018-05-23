5b3574c93de2c1166622b822b245b6faafabc424
sanketh kumar <sanketh.nm@gmail.com>
2018-05-24 01:39:59 +0530

    Contact form fix

--- script.js
+++ script.js
@@ -1,41 +1,65 @@
-
-	$(document).ready( function(){
-		$('.icon-anim').one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
+$(document).ready(function () {
+	$('.icon-anim').one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
 		console.log("animation end");
-		$(".icon-anim").css({"animation-dealy":"0s" , "-webkit-animation-delay" :"0s"});
-    });
-    
-    let $form = $('form');
-       $form.submit(function(){
+		$(".icon-anim").css({
+			"animation-dealy": "0s",
+			"-webkit-animation-delay": "0s"
+		});
+	});
 
-      alert($(this).attr('action')+'?'+"email="+$('#email').val()+"&subject="+$('#subject').val()+"&message="+$('#message').val());
-    //   $.get($(this).attr('action')+'?'+"email="+$('#email').val()+"&subject="+$('#subject').val()+"&message="+$('#message').val(), function(response,err){
-    //       console.log("okay"+response+err);
-    //        alert("success");
-    //   },'json');
-      
-      $('<i class="medium material-icons wow rotateIn" data-wow-delay="1s" style="color:green" >check_circle</i>').replaceAll('.submit');
-      M.toast({ html: "Email sent" , classes: 'rounded'});
-      return false;
-   });
-   
-	})
-	
-	function animateIt() {
-		console.log("click");
-		$(".icon-anim").removeClass("fadeInUp").addClass("fadeOutRight");
-		setTimeout(() => {
-			$(".icon-anim").removeClass("fadeOutRight").addClass("fadeInLeft");
-			console.log("time")
-		}, 1500);
+	let $form = $('form');
+	$form.submit(function () {
 
-		//$('.get_started').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
-			$('html,body').animate({
-					scrollTop: $(".skills").offset().top
-				},
-				1000);
-		//});
+		//alert($(this).attr('action') + '?' + "email=" + $('#email').val() + "&subject=" + $('#subject').val() + "&message=" + $('#message').val());
 
-    }
-    
-    
+		$.ajax({
+			url: $(this).attr('action') + '?' + "email=" + $('#email').val() + "&subject=" + $('#subject').val() + "&message=" + $('#message').val(),
+			dataType: 'text',
+			cache: false,
+
+			beforeSend: function () {
+				$('.submit').addClass('animated flip infinite')
+				console.log("Loading");
+			},
+
+			error: function (jqXHR, textStatus, errorThrown) {
+				console.log(jqXHR);
+				console.log(textStatus);
+				console.log(errorThrown);
+			},
+
+			success: function (data) {
+				console.log('Success');
+				console.log(data);
+				$('<i class="medium material-icons"  style="color:green" >check_circle</i>').replaceAll('.submit');
+				M.toast({
+					html: "Email sent",
+					classes: 'rounded'
+				});
+			},
+
+			complete: function () {
+				console.log('Finished all tasks');
+			}
+		});
+		return false;
+	});
+
+})
+
+function animateIt() {
+	console.log("click");
+	$(".icon-anim").removeClass("fadeInUp").addClass("fadeOutRight");
+	setTimeout(() => {
+		$(".icon-anim").removeClass("fadeOutRight").addClass("fadeInLeft");
+		console.log("time")
+	}, 1500);
+
+	//$('.get_started').on("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
+	$('html,body').animate({
+			scrollTop: $(".skills").offset().top
+		},
+		1000);
+	//});
+
+}
