1bd057a6161ea8468de42cee0838d108fbd62daf
sanketh kumar <sanketh.nm@gmail.com>
2018-05-23 20:27:44 +0530

    Added more info

--- script.js
+++ script.js
@@ -3,7 +3,22 @@
 		$('.icon-anim').one("animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd", function () {
 		console.log("animation end");
 		$(".icon-anim").css({"animation-dealy":"0s" , "-webkit-animation-delay" :"0s"});
-	});
+    });
+    
+    let $form = $('form');
+       $form.submit(function(){
+
+      alert($(this).attr('action')+'?'+"email="+$('#email').val()+"&subject="+$('#subject').val()+"&message="+$('#message').val());
+    //   $.get($(this).attr('action')+'?'+"email="+$('#email').val()+"&subject="+$('#subject').val()+"&message="+$('#message').val(), function(response,err){
+    //       console.log("okay"+response+err);
+    //        alert("success");
+    //   },'json');
+      
+      $('<i class="medium material-icons wow rotateIn" data-wow-delay="1s" style="color:green" >check_circle</i>').replaceAll('.submit');
+      M.toast({ html: "Email sent" , classes: 'rounded'});
+      return false;
+   });
+   
 	})
 	
 	function animateIt() {
@@ -21,4 +36,6 @@
 				1000);
 		//});
 
-	}
+    }
+    
+    
